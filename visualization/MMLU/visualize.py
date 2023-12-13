1b011b6ac889d2cfeee489283fc0a4f9b35e148d
Zichun Yu <53684662+yuzc19@users.noreply.github.com>
2023-12-13 13:07:54 -0500

    Add visualization for MMLU and Code (#8)
    * Add visualization for MMLU and Code
    
    * Update visualization for MMLU and Code

---
+++ visualization/MMLU/visualize.py
@@ -0,0 +1,54 @@
+from zeno_client import ZenoClient, ZenoMetric
+import pandas as pd
+import os
+import dotenv
+
+dotenv.load_dotenv("../.env", override=True)
+
+OUTPUT_DIR = "../../outputs/MMLU"
+
+models = os.listdir(OUTPUT_DIR)
+
+# Upload base dataset
+df = pd.read_json(os.path.join(OUTPUT_DIR, models[0], "output.jsonl"), lines=True)
+base_df = pd.DataFrame(
+    {
+        "task": df["task"],
+        "qid": df.index,
+        "question": df["prompt"].apply(lambda x: x.split("\n\n")[-1]),
+        "answer": df["label"],
+    }
+)
+
+zeno_client = ZenoClient(os.environ.get("ZENO_API_KEY"))
+
+project = zeno_client.create_project(
+    name="Gemini Evaluation - MMLU",
+    description="Evaluation of Gemini, GPT-4, and Mixtral on MMLU dataset",
+    view={
+        "data": {"type": "text"},
+        "label": {"type": "text"},
+        "output": {"type": "text"},
+    },
+    public=True,
+    metrics=[
+        ZenoMetric(name="Accuracy", type="mean", columns=["correct"]),
+    ],
+)
+
+project.upload_dataset(
+    base_df, id_column="qid", data_column="question", label_column="answer"
+)
+
+for model in models:
+    df = pd.read_json(os.path.join(OUTPUT_DIR, model, "output.jsonl"), lines=True)
+    output_df = pd.DataFrame(
+        {
+            "qid": df.index,
+            "output": df["prediction"],
+            "correct": df["correct"].astype(bool),
+        }
+    )
+    project.upload_system(
+        output_df, name=model, id_column="qid", output_column="output"
+    )
