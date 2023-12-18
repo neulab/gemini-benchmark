f68b16125881eb55252750f040b02ff4f1e139bf
Alex Bäuerle <alex@a13x.io>
2023-12-18 17:16:00 +0100

    update visualization scipts to load .env file (#21)

--- visualization/translation_flores/zeno_upload.py
+++ visualization/translation_flores/zeno_upload.py
@@ -1,15 +1,19 @@
-import pandas as pd
 import csv
 import os
+
+import dotenv
 import evaluate
+import pandas as pd
 from zeno_client import ZenoClient, ZenoMetric
 
+dotenv.load_dotenv("../.env", override=True)
+
 # Initialize Zeno client with your API key
-client = ZenoClient("API-KEY")
+client = ZenoClient("ZENO_API_KEY")
 
 # Directory paths
-data_dir = './data/flores200_dataset/devtest'
-system_outputs_dir = './exp_outputs_tsv'
+data_dir = "./data/flores200_dataset/devtest"
+system_outputs_dir = "./exp_outputs_tsv"
 trg_langs = ["ssw_Latn"]
 
 # Load CHRF evaluator
@@ -18,8 +22,9 @@ chrf_evaluator = evaluate.load("chrf")
 # Load reference data
 src_data, trg_data, trg_lang_data = [], [], []
 for trg_lang in trg_langs:
-    with open(os.path.join(data_dir, "devtest.eng_Latn"), "r") as f_src, \
-            open(os.path.join(data_dir, f"devtest.{trg_lang}"), "r") as f_trg:
+    with open(os.path.join(data_dir, "devtest.eng_Latn"), "r") as f_src, open(
+        os.path.join(data_dir, f"devtest.{trg_lang}"), "r"
+    ) as f_trg:
         src_lines = [line.strip() for line in f_src.readlines()]
         trg_lines = [line.strip() for line in f_trg.readlines()]
 
@@ -28,16 +33,20 @@ for trg_lang in trg_langs:
         trg_lang_data.extend([trg_lang] * len(src_lines))
 
 # Create the reference DataFrame
-df_ref = pd.DataFrame({"source": src_data, "target": trg_data, "trg_lang": trg_lang_data})
+df_ref = pd.DataFrame(
+    {"source": src_data, "target": trg_data, "trg_lang": trg_lang_data}
+)
 df_ref["id"] = df_ref.index
 
 # Upload reference data to Zeno
 project = client.create_project(
     name="Translation Evaluation",
     view="text-classification",
-    metrics=[ZenoMetric(name="CHRF", type="mean", columns=["chrf_score"])]
+    metrics=[ZenoMetric(name="CHRF", type="mean", columns=["chrf_score"])],
 )
-project.upload_dataset(df_ref, id_column="id", data_column='source', label_column="target")
+project.upload_dataset(
+    df_ref, id_column="id", data_column="source", label_column="target"
+)
 
 
 # Function to load predictions from TSV
@@ -50,8 +59,10 @@ def load_predictions_from_tsv(filename):
 
 # Function to calculate CHRF for each prediction
 def calculate_chrf(predictions, references):
-    return [chrf_evaluator.compute(predictions=[pred], references=[[ref]])["score"] for pred, ref in
-            zip(predictions, references)]
+    return [
+        chrf_evaluator.compute(predictions=[pred], references=[[ref]])["score"]
+        for pred, ref in zip(predictions, references)
+    ]
 
 
 # # Function to extract language and model details from filename
@@ -83,11 +94,15 @@ for trg_lang in trg_langs:
         chrf_scores = calculate_chrf(predictions, df_ref["target"])
 
         # Create DataFrame for system predictions
-        df_system = pd.DataFrame({
-            "output": predictions,
-            "chrf_score": chrf_scores,
-            "id": range(len(predictions)),
-        })
+        df_system = pd.DataFrame(
+            {
+                "output": predictions,
+                "chrf_score": chrf_scores,
+                "id": range(len(predictions)),
+            }
+        )
 
         # Upload system predictions to Zeno
-        project.upload_system(df_system, name=system_name, id_column="id", output_column="output")
+        project.upload_system(
+            df_system, name=system_name, id_column="id", output_column="output"
+        )
